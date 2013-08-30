1d5668be59ee19864c49a1e36364ae57d4e76086
billy-hardy <billy92@gmail.com>
2013-08-30 09:11:32 -0400

    Made more h files

---
+++ commhand.h
@@ -0,0 +1,14 @@
+#ifndef COMMHAND_H
+#define COMMHAND_H
+
+#include <stdio.h>
+#include <string.h>
+#include "help.c"
+
+#define TRUE 1
+#define NUM_COMMANDS 1
+
+void tokenizer(int *, char ***, char *);
+void commhand();
+
+#endif
