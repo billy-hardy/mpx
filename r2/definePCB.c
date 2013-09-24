7c714724be29f633d4a86822ada44e6b5872e56a
Locious <rbrown5@mix.wvu.edu>
2013-09-23 22:19:35 -0400

    Changes, not finished

--- r2/definePCB.c
+++ r2/definePCB.c
@@ -7,11 +7,45 @@ extern pcb_queue blocked;
 //Parameters: name, class and priority
 // name must be unique
 // class and priority must be valid
+
+//THIS HAS NOT BEEN COMPILED OR TESTED YET
 int createPCB(int argc, char **argv) {
+	pcb *tempPCB;
+	int classVal, priorityVal;
   if(argc != 4) {
     invalidArgs(argv[0]);
-  } else {
-    //TODO: stuff
+  }
+  else {
+	if((tempPCB=findPCB(argv[1])) == NULL){
+		classVal = integerCheck(argv[2]);
+		if(classVal != NULL){
+			priorityVal = integerCheck(argv[3]);
+			if(priority != NULL){
+				if (paramsGood(argv[1], classVal, priorityVal)){
+					tempPCB = allocatePCB();
+					tempPCB = setupPCB(argv[1], classVal, priorityVal); 
+					//Add to Queue here...which queue?
+				}
+				else{
+				//Parameters invalid (strlen, out of bounds ints)
+				}
+			}
+			else{
+			//invalid Priority Value (not an int)
+			}
+				
+		}
+		else{
+		//invalid Class Value (not an int)
+		}
+		
+	}
+	else{
+	//Duplicate Name
+	}
+		
+	
+	
   }
   return LOOP;
 }
@@ -25,4 +59,16 @@ int deletePCB(int argc, char **argv) {
     //TODO: stuff
   }
   return LOOP;
+}
+
+
+void integerCheck(char *in){
+	double checkVal;
+	int returnVal = NULL;
+	checkVal = atof(in);
+	if(checkVal%1 > 0);
+	else{
+		returnVal = (int)checkVal;
+	}
+	return returnVal;
 }
