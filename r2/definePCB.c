4bcb801ce541e57f5464d7cd3d942d0da039be58
Locious <rbrown5@mix.wvu.edu>
2013-09-24 23:48:54 -0400

    Fixed...I think

--- r2/definePCB.c
+++ r2/definePCB.c
@@ -18,51 +18,67 @@ int createPCB(int argc, char **argv) {
     invalidArgs(argv[0]);
   }
   else {
-	if((tempPCB=findPCB(argv[1])) == NULL){
-		if((classVal= integerCheck(argv[2])) != NULL){
-			if((priority = integerCheck(argv[3]))!= NULL){
-				if (paramsGood(argv[1], classVal, priorityVal)){
-					tempPCB = allocatePCB();
-					tempPCB = setupPCB(argv[1], classVal, priorityVal); 
-					insertPCB(tempPCB);
+		if((tempPCB=findPCB(argv[1])) == NULL){
+			if((classVal= integerCheck(argv[2])) != NULL){
+				if((priority = integerCheck(argv[3]))!= NULL){
+					if (paramsGood(argv[1], classVal, priorityVal)){
+						tempPCB = allocatePCB();
+						tempPCB = setupPCB(argv[1], classVal, priorityVal); 
+						insertPCB(tempPCB);
 					
 					
 					//********************Are there error codes for these somewhere.....
+					}
+					else{
+					//Parameters invalid (strlen, out of bounds ints)
+					strcpy(messageBuffer, "Invalid Parameters!  Use \"help\" for more information.\n");
+					bufferSize = strlen(messageBuffer);
+					sys_req(WRITE, TERMINAL, messageBuffer, &bufferSize);
+					}
 				}
 				else{
-				//Parameters invalid (strlen, out of bounds ints)
-				strcpy(messageBuffer, "Invalid Parameters!  Use \"help\" for more information.\n");
-				bufferSize = strlen(messageBuffer);
-				sys_req(WRITE, TERMINAL, messageBuffer, &bufferSize);
-				
+				//invalid Priority Value (not an int)
+					strcpy(messageBuffer, "Invalid Priority Value!  Use \"help\" for more information.\n");
+					bufferSize = strlen(messageBuffer);
+					sys_req(WRITE, TERMINAL, messageBuffer, &bufferSize);
 				}
+				
 			}
 			else{
-			//invalid Priority Value (not an int)
-				strcpy(messageBuffer, "Invalid Priority Value!  Use \"help\" for more information.\n");
+		//invalid Class Value (not an int)
+				strcpy(messageBuffer, "Invalid Class Value!  Use \"help\" for more information.\n");
 				bufferSize = strlen(messageBuffer);
 				sys_req(WRITE, TERMINAL, messageBuffer, &bufferSize);
-			}
-				
+				}
+		
 		}
 		else{
-		//invalid Class Value (not an int)
-			strcpy(messageBuffer, "Invalid Class Value!  Use \"help\" for more information.\n");
+		//Duplicate Name
+			strcpy(messageBuffer, "Invalid Name! Process of this name already exists! Use \"help\" for more information.\n");
 			bufferSize = strlen(messageBuffer);
 			sys_req(WRITE, TERMINAL, messageBuffer, &bufferSize);
 		}
-		
 	}
-	else{
-	//Duplicate Name
-		strcpy(messageBuffer, "Invalid Name! Process of this name already exists! Use \"help\" for more information.\n");
-		bufferSize = strlen(messageBuffer);
-		sys_req(WRITE, TERMINAL, messageBuffer, &bufferSize);
-    }
-  return LOOP;
-	}
+  return LOOP;	
 }
 
+int deletePCB(int argc, char **argv) {  //Handle a PCB that is currently running??
+	pcb *tempPCB;
+  if(argc != 2) {
+    invalidArgs(argv[0]);
+  }
+  else {  
+	if((tempPCB = findPCB(argv[1]))!= NULL){
+		removePCB(tempPCB); //This will work once the function is changed.
+		freePCB(tempPCB);
+	}
+	else{
+		printError(PCB_NOT_FOUND);
+	}
+  }
+  return LOOP;
+}
+	
 
 int integerCheck(char *in){
   double checkVal;
