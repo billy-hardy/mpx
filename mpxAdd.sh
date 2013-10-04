65e97d2c122089f1086777f725fa330fd76c04b7
Billy Hardy <billy92@gmail.com>
2013-10-04 06:00:52 -0400

    help and history are perfect.

--- mpxAdd.sh
+++ mpxAdd.sh
@@ -3,21 +3,34 @@ git add mpxAdd.sh
 
 git add r1/commhand.c
 git add r1/date.c
-git add r1/help/date.txt
 git add r1/exitMPX.c
-git add r1/help/exit.txt
 git add r1/help.c
-git add r1/help/help.txt
 git add r1/invalidArgs.c
 git add r1/ls.c
-git add r1/help/ls.txt
 git add r1/main.c
 git add r1/mpx_supt.c
 git add r1/r1.h
-git add r1/help/rename.txt
 git add r1/version.c
-git add r1/help/version.txt
 git add r1/history.c
+
+git add r1/help/date.txt
+git add r1/help/exit.txt
+git add r1/help/help.txt
+git add r1/help/ls.txt
+git add r1/help/rename.txt
+git add r1/help/version.txt
+git add r1/help/history.txt
+git add r1/help/create.txt
+git add r1/help/delete.txt
+git add r1/help/block.txt
+git add r1/help/blocked.txt
+git add r1/help/priority.txt
+git add r1/help/ready.txt
+git add r1/help/resume.txt
+git add r1/help/showAll.txt
+git add r1/help/showPCB.txt
+git add r1/help/suspend.txt
+git add r1/help/unblock.txt
 
 git add r2/PCBProcedures.c
 git add r2/alterPCB.c
