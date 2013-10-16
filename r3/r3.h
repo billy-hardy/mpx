fc9efe6784e02a21e68634573976158204724863
Billy Hardy <billy92@gmail.com>
2013-10-16 18:26:47 +0000

    dispatch and sys_call done

--- r3/r3.h
+++ r3/r3.h
@@ -1,26 +1,31 @@
 #include <dos.h>
 
-unsigned char* MK_FP;
-unsigned char FP_SEG();
-unsigned char FP_OFF();
-//removed the void* from seg and off
-
-
 //Prototypes
 void interrupt dispatch();
 void interrupt sys_call();
 //?
-void loadProcess();
-void callDispatch();
-void loadTestProcess();
+void load_procs(char *, int, int, void *);
+void r3Init();
 
+//temp commands
+int callDispatch(int, char **);
+int loadTestProcess(int, char **);
+
+void mkFPStackTop(unsigned char *);
 
 #define SYS_STACK_SIZE 1024
 
-typedef struct params{
-	int op_code;
-	int device_id;
-	unsigned char *buff_addr;
-	int *count_addr;
-}params;
+typedef struct params {
+  int op_code;
+  int device_id;
+  unsigned char *buff_addr;
+  int *count_addr;
+} params;
 
+typedef struct context {
+  unsigned int BP, DI, SI, DS, ES;
+  unsigned int DX, CX, BX, AX;
+  unsigned int IP, CS, FLAGS;
+} context;
+
+extern pcb *running;
