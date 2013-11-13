df78ab1f77ba946a3c3a778237c83568539265c9
Billy Hardy <billy92@gmail.com>
2013-11-13 18:23:21 +0000

    batch should be fixed

--- r1/exec.c
+++ r1/exec.c
@@ -6,35 +6,34 @@ int exec(int argc, char **argv) {
   if(argc != 1) {
     int i, j, numCommands=0, fLength;
     FILE *fptr;
-    char *buffer, errorMessage[128], *commands[129];
+    char *buffer, errorMessage[128], *command;
     int errorMessageSize;
     for(i=1; i<argc; i++) {
       fptr = fopen(argv[i], "r");
-			if(fptr != NULL) {
-				fseek(fptr, 0L, SEEK_END);
-				fLength = ftell(fptr);
-				fseek(fptr, 0L, SEEK_SET);
-				//rewind(fptr);
-				buffer = (char *)sys_alloc_mem(fLength*sizeof(char));
-				fread(buffer, 1, fLength, fptr);
-				tokenize(&numCommands, commands, buffer, delims);
-				for(j=0; j<numCommands; j++) {
-					returnVal = eval(commands[j]);
-					if(!returnVal) break;
-				}
-				for(j=0; j<numCommands; j++) {
-					commands[j][0] = '\0';
-				}
-			} else {
-				strcpy(errorMessage, "Invalid file.\n");
-				errorMessageSize = strlen(errorMessage);
-				sys_req(WRITE, TERMINAL, errorMessage, &errorMessageSize);
-			}
-			sys_free_mem(buffer);
-			fclose(fptr);
-		} 
-	} else {
-		invalidArgs(argv[0]);
+      if(fptr != NULL) {
+	fseek(fptr, 0L, SEEK_END);
+	fLength = ftell(fptr);
+	fseek(fptr, 0L, SEEK_SET);
+	buffer = (char *)sys_alloc_mem(fLength*sizeof(char));
+	fread(buffer, 1, fLength, fptr);
+	command = strtok(buffer, delims);
+	for(j=0; j<numCommands; j++) {
+	  if(strlen(command) < 127) {
+	    returnVal = eval(commands[j]);
+	    if(!returnVal) break;
+	    else command = strtok(NULL, delims);
+	  }
 	}
-	return returnVal;
+      } else {
+	strcpy(errorMessage, "Invalid file.\n");
+	errorMessageSize = strlen(errorMessage);
+	sys_req(WRITE, TERMINAL, errorMessage, &errorMessageSize);
+      }
+      sys_free_mem(buffer);
+      fclose(fptr);
+    } 
+  } else {
+    invalidArgs(argv[0]);
+  }
+  return returnVal;
 }
