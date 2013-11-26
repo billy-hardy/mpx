19c23cbbd953e407c8f051e0106abf77e8b15dc7
Billy Hardy <billy92@gmail.com>
2013-11-26 07:49:15 +0000

    moved r6 stuff into r3.c/h

--- r3/r3.c
+++ r3/r3.c
@@ -1,5 +1,7 @@
 #include "r3.h"
 
+params *param_ptr;
+
 void load_procs(pcb *np, context *npc, void (*func) (void)) {
   npc->IP = FP_OFF(func);
   npc->CS = FP_SEG(func);
@@ -48,4 +50,63 @@ int callDispatch(int argc, char *argv[]) {
     dispatch();
   }
   return LOOP;
+}
+
+void interrupt dispatch() {
+  if(ss_save == NULL) {
+    //save stack pointers
+    ss_save = _SS;
+    sp_save = _SP;
+  }
+  running = getNextRunning();
+  if(running != NULL) {
+    removePCB(running);
+    running->state = RUNNING;
+    new_ss = FP_SEG(running->top);
+    new_sp = FP_OFF(running->top);
+    _SS = new_ss;
+    _SP = new_sp;
+  } else {
+    //restore stack pointer
+    _SS = ss_save;
+    _SP = sp_save;
+    ss_save = NULL;
+    sp_save = NULL;
+  }
+}
+
+void interrupt sys_call() {
+  running->top = MK_FP(_SS, _SP);
+
+  //switch to temp stack
+  new_ss = FP_SEG(&sys_stack);
+  new_sp = FP_OFF(&sys_stack) + SYS_STACK_SIZE;
+  _SS = new_ss;
+  _SP = new_sp;
+
+  param_ptr = (params *) (running->top+sizeof(context));
+  if(running != NULL) {
+    switch(param_ptr->op_code) {
+    case(IDLE):
+      running->state = READY;
+      insertPCB(running);
+      break;
+    case(EXIT):
+      freePCB(running);
+      running = NULL;
+      break;
+    }
+  }
+  dispatch();
+}
+
+
+void r3Init() {
+  sys_set_vec(sys_call);
+  ss_save = NULL;
+  sp_save = NULL;
+}
+
+int io_scheduler() {  
+  
 }
